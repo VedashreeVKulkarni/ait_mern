c0c6f81089234208386e9dc338248b751e475c7e
Vedashree V Kulkarni <vedashree369@gmail.com>
2025-05-29 13:02:49 +0530

    commit

--- person/index.js
+++ person/index.js
@@ -1,3 +1,20 @@
-import{a,b}from'.p1.js';
-console.log(a);
-console.log(b);
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+// const bodyParser = require('body-parser');
+const port = 3000;
+
+const mongoURI = 'mongodb://localhost:27017/mydatabase'; // Replace with your MongoDB URI
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+    res.send('The response just came from backend!');
+  });
+
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+    });
